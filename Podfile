bb6faafb3b3fd9afc7ebaf1036a5407bbf85b2a1
Sergey Zolotarev <sergey.zolotarev@rhinoda.com>
2016-01-25 16:51:56 +0600

    Make text field visible after keyboard shows up

--- Podfile
+++ Podfile
@@ -1,8 +1,10 @@
 platform :ios, '8.0'
 use_frameworks!
+inhibit_all_warnings!
 
 target 'Weather' do
     pod 'Alamofire', '~> 3.0'
+    pod 'IQKeyboardManagerSwift', '~> 3.3.4'
 end
 
 link_with 'Weather'
--- Weather/AppDelegate.swift
+++ Weather/AppDelegate.swift
@@ -6,14 +6,16 @@
 //  Copyright © 2016 Rhinoda. All rights reserved.
 //
 
+import IQKeyboardManagerSwift
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        IQKeyboardManager.sharedManager().enable = true
         return true
     }
 }
